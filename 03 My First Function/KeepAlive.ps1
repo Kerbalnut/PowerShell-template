76a63676145a3a4810bf40e2d444111a29a7476f
Kerbalnut <Kerbalnut@users.noreply.github.com>
2022-03-24 14:40:43 -0700

    Add more detail to KeepAlive.ps1 script.

--- 03 My First Function/KeepAlive.ps1
+++ 03 My First Function/KeepAlive.ps1
@@ -1,12 +1,15 @@
 
-Clear-Host
-Write-Host "Keep host alive with scroll lock"
-
 $WShell = New-Object -ComObject "WScript.shell"
 
+$i = 0
+$StartTime = Get-Date
 while ($True) {
+	$i++
+	Clear-Host
+	Write-Host "Keep host alive with scroll lock: (Round $i) Start time: $StartTime"
 	$WShell.sendkeys("{SCROLLLOCK}")
 	Start-Sleep -Milliseconds 100
 	$WShell.sendkeys("{SCROLLLOCK}")
 	Start-Sleep -Seconds 240
 }
+
