a844a548ad79eaf8e92cbb5f768b6ea4f9da45bd
Kerbalnut <Kerbalnut@users.noreply.github.com>
2022-04-10 21:49:19 -0700

    Add ProgressBar test functions.

--- 04 Module Template/ModuleTemplate/Start-SleepTimer.ps1
+++ 04 Module Template/ModuleTemplate/Start-SleepTimer.ps1
@@ -117,6 +117,7 @@ Function Start-SleepTimer {
 		
 	}
 	
+	#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	$Method = 0
 	switch ($Method) {
@@ -152,6 +153,33 @@ Function Start-SleepTimer {
 			} # End Function Get-HeaderSpacer
 			
 			$HeaderLineBreaks = Get-HeaderSpacer -HeaderBreaks $HeaderBreaks
+			
+			Function Get-ProgressBarTest {
+				<#
+				.LINK
+				https://thinkpowershell.com/how-to-make-a-powershell-progress-bar/
+				#>
+				For ($i=0; $i -le 100; $i++) {
+					Start-Sleep -Milliseconds 20
+					Write-Progress -Activity "Counting to 100" -Status "Current Count: $i" -PercentComplete $i -CurrentOperation "Counting ..."
+				}
+			} # End Function Get-ProgressBarTest
+			
+			Function Get-NestedProgressBarTest {
+				<#
+				.LINK
+				https://thinkpowershell.com/how-to-make-a-powershell-progress-bar/
+				#>
+				For ($i=0; $i -le 100; $i++) {
+					Start-Sleep -Milliseconds 1
+					Write-Progress -Id 1 -Activity "First Write Progress" -Status "Current Count: $i" -PercentComplete $i -CurrentOperation "Counting ..."
+					
+					For ($j=0; $j -le 100; $j++) {
+						Start-Sleep -Milliseconds 1
+						Write-Progress -Id 2 -Activity "Second Write Progress" -Status "Current Count: $j" -PercentComplete $j -CurrentOperation "Counting ..."
+					}
+				}
+			} # End Function Get-NestedProgressBarTest
 			
 			
 			
